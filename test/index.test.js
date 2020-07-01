2f80d4abfc2b15973540594fca68879a263b965c
SupinePandora43 <supinepandoraggg@gmail.com>
2020-07-01 13:41:26 +0500

    tests

---
+++ test/index.test.js
@@ -0,0 +1,42 @@
+const test = require("zora").test
+const posts = require("../lib/index").posts
+
+test("lol", async (t) => {
+    let lol = await posts({ tags: ["sfw"] })
+    console.log(lol[0])
+    t.equal(lol[0], {
+        height: 1707,
+        score: 1,
+        file_url: 'https://himg.rule34.xxx/images/3457/ddb7abf29ea807c55c1109ba78ceb935.png',
+        parent_id: '',
+        sample_url: 'https://rule34.xxx/samples/3457/sample_ddb7abf29ea807c55c1109ba78ceb935.jpg',
+        sample_width: 850,
+        sample_height: 1134,
+        preview_url: 'https://rule34.xxx/thumbnails/3457/thumbnail_ddb7abf29ea807c55c1109ba78ceb935.jpg',
+        rating: 's',
+        tags: ' cute no_color sfw slime_girl suraimu suraimu(hyper_cawk) ',
+        id: 3897419,
+        width: 1280,
+        change: 1593467420,
+        md5: 'ddb7abf29ea807c55c1109ba78ceb935',
+        creator_id: 709956,
+        has_children: false,
+        created_at: 'Mon Jun 29 21:48:39 +0000 2020',
+        status: 'active',
+        source: '',
+        has_notes: false,
+        has_comments: false,
+        preview_width: 112,
+        preview_height: 150,
+        tags_parsed: [
+            '',
+            'cute',
+            'no_color',
+            'sfw',
+            'slime_girl',
+            'suraimu',
+            'suraimu(hyper_cawk)',
+            ''
+        ]
+    })
+})
